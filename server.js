6755b6037fbde49520befccc7a114ca90db6e1b8
Brandon Robinson <blrobinson@Brandons-iMac.local>
2016-10-09 00:02:46 -0700

    Trying to set deploy on heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 'use strict';
 
 // Set the 'NODE_ENV' variable
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 /* Load dependencies */
 var express = require('./config/express');
@@ -10,10 +10,18 @@ var express = require('./config/express');
 // Create a new Express application instance
 var app = express();
 
-/* Use the Express application instance to listen on localhost */
-app.listen(3000);
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-console.log('Server running at http://localhost:3000/');
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
+
+// /* Use the Express application instance to listen on localhost */
+// app.listen(3000);
+
+// console.log('Server running at http://localhost:3000/');
 
 /* Expose this application */
 module.exports = app;
