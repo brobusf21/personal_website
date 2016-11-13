8a483e1fd741914db00047b13195ead20cbe8643
Brandon Robinson <blrobinson@Brandons-iMac.local>
2016-11-12 21:37:09 -0800

    Cleaning up shop

--- public/blog/config/blog.client.routes.js
+++ public/blog/config/blog.client.routes.js
@@ -8,23 +8,35 @@ angular.module('blog').config(['$routeProvider',
 		when('/blog', {
 			templateUrl: 'blog/views/blog.client.view.html'
 		}).
-		when('/blog/unixshell', {
-			templateUrl: 'blog/views/articles/unixshell.client.view.html'
+		when('/blog/technology', {
+			templateUrl: 'blog/views/articles/technology/blog-tech-index.client.view.html'
 		}).
-		when('/blog/herokudeploy', {
-			templateUrl: 'blog/views/articles/herokudeploy.client.view.html'
+		when('/blog/cycling', {
+			templateUrl: 'blog/views/articles/cycling/blog-cycling-index.client.view.html'
 		}).
-		when('/blog/grep', {
-			templateUrl: 'blog/views/articles/grep.client.view.html'
+		when('/blog/lifestyle', {
+			templateUrl: 'blog/views/articles/lifestyle/blog-lifestyle-index.client.view.html'
 		}).
-		when('/blog/mlab', {
-			templateUrl: 'blog/views/articles/mlab.client.view.html'
+		when('/blog/business', {
+			templateUrl: 'blog/views/articles/business/blog-business-index.client.view.html'
 		}).
-		when('/blog/logistics-business', {
-			templateUrl: 'blog/views/articles/logistics-business.client.view.html'
+		when('/blog/technology/unixshell', {
+			templateUrl: 'blog/views/articles/technology/unixshell.client.view.html'
 		}).
-		when('/blog/moving-to-austin', {
-			templateUrl: 'blog/views/articles/moving-to-austin.client.view.html'
+		when('/blog/technology/heroku', {
+			templateUrl: 'blog/views/articles/technology/heroku.client.view.html'
+		}).
+		when('/blog/technology/grep', {
+			templateUrl: 'blog/views/articles/technology/grep.client.view.html'
+		}).
+		when('/blog/technology/mlab', {
+			templateUrl: 'blog/views/articles/technology/mlab.client.view.html'
+		}).
+		when('/blog/business/logistics-business', {
+			templateUrl: 'blog/views/articles/business/logistics-business.client.view.html'
+		}).
+		when('/blog/lifestyle/moving-to-austin', {
+			templateUrl: 'blog/views/articles/lifestyle/moving-to-austin.client.view.html'
 		}).
 		otherwise({
 			redirectTo: '/'
